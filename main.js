58c63370e2191ce81a65e9ecf00de4bf5ea20120
Thuotracy <tracyjacobs775@gmail.com>
2022-09-07 17:00:35 +0300

    made a few changes to main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const container = document.querySelector('.container')
-for  (var i = 0; i <= 5; i++){
+for (var i = 0; i <= 100; i++){
     const blocks = document.createElement('div');
     blocks.classList.add('block');
     container.appendChild(blocks)
 }
+
+function animateBlocks(){
+    anime({
+        targets: '.block',
+        translateX: function(){
+            return anime.random(-700,700);
+        },
+        translateY: function(){
+            return anime.random(-500,500);
+        },
+    })
+}
+
+animateBlocks()
